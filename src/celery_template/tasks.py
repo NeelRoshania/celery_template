5118d820225274eb6d2f20614380df8178e9977c
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-09 01:03:13 -0500

    testing get_task_logger()
    https://docs.celeryq.dev/en/latest/userguide/tasks.html#logging

--- src/celery_template/tasks.py
+++ src/celery_template/tasks.py
@@ -1,7 +1,11 @@
 from celery_template import app
+from celery.utils.log import get_task_logger
+
+clogger = get_task_logger(__name__)
 
 @app.task
 def add(x, y):
+    clogger.info('Adding {0} + {1}'.format(x, y))
     return x + y
 
 @app.task

9b9afef89b64a034ae79e366da243bcbf5d4f427
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-18 03:48:42 -0500

    Update funcs.py

--- src/celery_template/funcs.py
+++ src/celery_template/funcs.py
@@ -1,8 +1,8 @@
 import logging
 # from celery_template import logging
 
-LOGGER = logging.getLogger(__name__) # this calls the celery_template.funcs logger
 logging.config.fileConfig('conf/logging.conf', defaults={'fileHandlerLog': f'logs/{__name__}.log'}) # this will call celery_template.funcs
+LOGGER = logging.getLogger(__name__) # this calls the celery_template.funcs logger
 
 def specific_func(text:str) -> None:
 

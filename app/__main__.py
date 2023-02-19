f3679286f7f306d2bc2f27902bf09ee00a97356b
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-18 22:22:59 -0500

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -6,7 +6,7 @@ LOGGER = logging.getLogger(__name__) # this logger is defined seperately, see lo
 if __name__ == "__main__":
     
     # run celery worker as a subprocess
-    LOGGER.info(f'starting celery app worker service - app:celery_template, queue: celery_template_queue, worker:worker')
+    LOGGER.info(f'starting celery worker node - app:celery_template, queue: celery_template_queue, worker:worker')
 
     subprocess.Popen(
                         [

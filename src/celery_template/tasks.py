41a79b5a4b4a8cd9ba9004572941fd08e00b5778
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-18 20:46:45 -0500

    Update tasks.py

--- src/celery_template/tasks.py
+++ src/celery_template/tasks.py
@@ -7,6 +7,10 @@ from celery.utils.log import get_task_logger
 # logger = get_task_logger(__name__) # this should call the logger celery_template.tasks
 logger = logging.getLogger(__name__) # this should call the logger celery_template.tasks
 
+@celery.signals.setup_logging.connect
+def on_celery_setup_logging(**kwargs):
+    pass
+
 @app.task(bind=True)
 def add(self, x, y):
     logger.info(f'task_id:{self.request.id}, task_group:{self.request.group} - args=({x}, {y})') # can't get celery.utils.log.get_task_logger to work

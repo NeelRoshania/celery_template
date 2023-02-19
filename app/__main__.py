43aaa53236c4d40055112989282947ce7a2bff6a
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-18 21:28:01 -0500

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
                             "worker",
                             "-Q",
                             "celery_template_queue",
+                            "-f celery.log"
                             "--loglevel=DEBUG"
                         ]
                     )

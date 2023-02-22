6db28ad95492012b3aaede484d402f5e9dac931f
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-21 21:37:09 -0500

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import subprocess
 import logging
 
@@ -5,18 +6,40 @@ LOGGER = logging.getLogger(__name__) # this logger is defined seperately, see lo
 
 if __name__ == "__main__":
     
-    # run celery worker as a subprocess
-    LOGGER.info(f'starting celery worker node - app:celery_template, queue: celery_template_queue, worker:worker')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str, action="store", default="conf/config.yaml", nargs="?") # need to define this
+    parser.add_argument("--flower", "-f", const='', action="store_const")
+    args = parser.parse_args()
 
-    subprocess.Popen(
-                        [
-                            "celery",
-                            "-A",
-                            "celery_template",
-                            "worker",
-                            "-Q",
-                            "celery_template_queue",
-                            "--loglevel=INFO",
-                            # "--logfile=logs/celery.log"
-                        ]
-                    )
+    # run celery worker as a subprocess    
+    if args.flower:
+        LOGGER.info(f'starting celery worker node with flower - app:celery_template, queue: celery_template_queue, worker:worker')
+        subprocess.Popen(
+                            [
+                                "celery",
+                                "-A",
+                                "celery_template",
+                                "worker",
+                                "-Q",
+                                "celery_template_queue",
+                                "--loglevel=INFO",
+                                "flower",
+                                "--port=5566"
+                                # "--logfile=logs/celery.log"
+                            ]
+                        )
+    else:
+        LOGGER.info(f'starting celery worker node - app:celery_template, queue: celery_template_queue, worker:worker')
+
+        subprocess.Popen(
+                            [
+                                "celery",
+                                "-A",
+                                "celery_template",
+                                "worker",
+                                "-Q",
+                                "celery_template_queue",
+                                "--loglevel=INFO",
+                                # "--logfile=logs/celery.log"
+                            ]
+                        )
